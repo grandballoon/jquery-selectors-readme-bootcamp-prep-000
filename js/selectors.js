bbf3a535ecc0ece0fe92647b85a87f5d6ced729b
Luke Conley <luke.sovern.conley@gmail.com>
2018-02-24 18:05:08 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -21,6 +21,6 @@ function divSelector(){
 
 function firstListItem(){
   $('parent-tag child-tag:first-child');
-  let listItem = $('ul pic-list:first-child');
+  let listItem = $('ul #pic-list:first-child');
   return listItem;
 }
